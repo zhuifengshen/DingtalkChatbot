b7d0ef8cfeca710d3f80cb8f957b063b487750ac
devin <zhangchuzhao@youmi.net>
2018-08-24 21:17:09 +0800

    chore: 支持python setup.py uplaod一键上传PIPY功能

---
+++ dingtalkchatbot/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'DingtalkChatbot'
+__description__ = '一个钉钉自定义机器人消息的Python封装库'
+__url__ = 'https://github.com/zhuifengshen/DingtalkChatbot'
+__version__ = '1.2.0'
+__author__ = 'devin'
+__author_email__ = '1324556701@qq.com'
+__license__ = 'MIT'
+__cake__ = u'\u2728 \U0001f370 \u2728'
--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
-# create time: 13/01/2018 20:41
 import io
-from setuptools import setup, find_packages
+import os
+import sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'dingtalkchatbot', '__about__.py'), encoding='utf-8') as f:
+    exec(f.read(), about)
 
 with io.open("README.rst", encoding='utf-8') as f:
     long_description = f.read()
@@ -10,17 +18,61 @@ with io.open("README.rst", encoding='utf-8') as f:
 with io.open("requirements.txt", encoding='utf-8') as f:
     install_requires = f.readlines()
 
+
+class UploadCommand(Command):
+    """ Build and publish this package.
+        Support setup.py upload. Copied from requests_html.
+    """
+
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in green color."""
+        print("\033[0;32m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        """ override
+        """
+        pass
+
+    def finalize_options(self):
+        """ override
+        """
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+            rmtree(os.path.join(here, 'DingtalkChatbot.egg-info'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Publishing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
 setup(
-    name="DingtalkChatbot",
-    version="1.1.3",
+    name=about['__title__'],
+    version=about['__version__'],
     packages=find_packages(),
-    description="DingtalkChatbot is a Python wrapper tool for dingtalk custom chatbot messages.",
+    description=about['__description__'],
     long_description=long_description,
-    author="Devin Zhang",
-    author_email="1324556701@qq.com",
-    url="https://github.com/zhuifengshen/DingtalkChatbot",
-    license='MIT',
-    keywords='dingtalk dingding chatbot robot bot',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    license=about['__license__'],
+    keywords='钉钉 机器人 dingtalk chatbot robot bot',
     install_requires=install_requires,
     setup_requires=['setuptools'],
     classifiers=[
@@ -28,6 +80,11 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    # python setup.py upload
+    cmdclass={
+        'upload': UploadCommand
+    }
 )
